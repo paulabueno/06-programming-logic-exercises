e7553e2756acb942454890b8909347c43d2daab3
paulabueno <paulabueno.ferreira1@gmail.com>
2023-02-17 16:35:32 +0100

    a * b tabuada

---
+++ exercicio-05/script.js
@@ -0,0 +1,9 @@
+for (a = 1; a <= 10; a++){
+    console.log(`tabuada de ${a}`);
+    for (b = 1; b <= 10; b++){
+        const result = a * b;
+        console.log(`${a} x ${b} = ${result}`);
+    }
+console.log("")
+}
+
